c53f76241ce855b075ad12d5aaa9c3533d09c3b3
Nathan K <code200tech@gmail.com>
2023-06-14 22:46:30 -0700

    setup basic redux structure and setup prettier

---
+++ src/components/CarList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CarList() {
+  return <>List of Cars</>;
+}
+
+export default CarList;
---
+++ src/components/CarSearch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CarSearch() {
+  return <>Car Search Form</>;
+}
+
+export default CarSearch;
